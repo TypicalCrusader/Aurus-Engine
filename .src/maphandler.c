aa1bbc9c39bceae65b13ca3c3c6fb771bf0d557d
TypicalCrusader <janflisek@gmail.com>
2024-03-21 12:58:18 +0100

    fixed lidicrous stack func

--- .src/maphandler.c
+++ .src/maphandler.c
@@ -1,17 +1,18 @@
 ﻿#include "../include/map.h"
 
-void IncreaseTurnCounter(CurrentMap Map)
+static void IncreaseTurnCounter(CurrentMap Map)
 {
     int Currentturn = Map.CurrentTurn;
-    int * Currentturn = (int*)malloc(n * sizeof(Map));
+    int * Currentturn = (int*)malloc(1 * sizeof(Map));
 
     if (Currentturn != NULL) {
         Currentturn += 1;
+        Map.CurrentTurn = Currentturn;
         return;
-        free;
+        free(Currentturn);
     }
     else {
-        free; //even though no memory was assigned its still best to clear the mem
+        free(Currentturn); //even though no memory was assigned its still best to clear the mem, juuuust in case
         return -1;
     }
 };
